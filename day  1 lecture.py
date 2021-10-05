f41c9a441f82331c62f3af0afd647318bb91b2b3
sana-dev <55602404+sana-dev@users.noreply.github.com>
2021-10-05 19:27:29 +0200

    Add files via upload

---
+++ day  1 lecture.py
@@ -0,0 +1,23 @@
+print("hello\nhelloooo")    
+#string by adding /n it prints on the next line
+print("hello"+"sara")
+# if we add + in between in will print both words together
+print("hello"+" "+"sara")
+# here we write the + and emply commas space inbetween them so there will be space when we run it
+print("hello" + input("what is your name?"))
+# here when we write then i output u can write something and after that you are using print so it will print together 
+input("what is your name?")
+# we write input to give the input option in output
+print(len(input("what is your name?")))
+# we are usings "len" to count the number of characters inside the ipput provided by the user
+name = input("what is your name?")
+length = len(name)
+print(length)
+# we can give names that are variables in python to different commands so we can use it later in code so it is easy to write 
+a = input ("a:")
+b = input ("b:")
+
+
+
+print("a =" + a)
+print("b =" + b)
